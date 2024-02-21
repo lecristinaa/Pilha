4b0df9d6352bc1ea406f3302f662acb01ca72fd5
lê <112022669+lecristinaa@users.noreply.github.com>
2024-02-21 12:45:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-import { iniciarPilha } from "./pilha.js"
-import { iniciarPilha, inserirPilha } from "./pilha.js"
+// type: module --> importa e exporta os códigos de arquivos diferentes
+// type: commonjs -->  não consegue utilizar importar e exportar
 
-const pilha = iniciarPilha()
+import { criarPilha, inserirItem, retirarItem } from "./pilha.js"
+
+const pilha = criarPilha()
+
+
+
+// Inserir item na pilha manualmente
+// inserirItem(pilha, "Haroldo")
+// inserirItem(pilha, "Isabelle")
+
+// Inserir item na pilha com nova array
+const nomesAlunos = [
+    "Haroldo",
+    "Isabelle",
+    "Juliana",
+    "Caio",
+]
+nomesAlunos.forEach((nome) => {
+    inserirItem(pilha, nome)
+})
+
+
+// Retirar item da pilha e conseguir mostrar quem foi removido
+// retirarItem(pilha) --> apenas retirar, quando a função não tiver return
+
+
+
 console.log(pilha) // Exibe no console a array com X elementos
-
-inserirPilha(pilha, "Haroldo")
-console.log(pilha)
